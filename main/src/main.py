7fddd22e7271eb1555ce953f7c6223c9b1937c7c
Deepesh Gupta <deepesh@findcoder.io>
2024-02-06 15:31:43 +0530

    error handle

--- main/src/main.py
+++ main/src/main.py
@@ -1,36 +1,34 @@
-from appwrite.client import Client
 from youtube_transcript_api import YouTubeTranscriptApi
-import os
-
 
 # This is your Appwrite function
 # It's executed each time we get a request
 def main(context):
-    # Why not try the Appwrite SDK?
-    #
-    # client = (
-    #     Client()
-    #     .set_endpoint("https://cloud.appwrite.io/v1")
-    #     .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-    #     .set_key(os.environ["APPWRITE_API_KEY"])
-    # )
+    try:
+        # The `ctx.req` object contains the request data
+        if context.req.method == "GET":
+            videoId = context.req.query.get("videoId")
+            
+            if videoId is None:
+                raise ValueError("videoId parameter is missing")
 
-    # The `ctx.req` object contains the request data
-    if context.req.method == "GET":
-        videoId = context.req.query["videoId"]
-        transcript = YouTubeTranscriptApi.get_transcript(videoId)
+            transcript = YouTubeTranscriptApi.get_transcript(videoId)
 
-        context.log(transcript)
-        # Send a response with the res object helpers
-        # `ctx.res.send()` dispatches a string back to the client
-        return context.res.json(transcript)
+            context.log(transcript)
+            # Send a response with the res object helpers
+            # `ctx.res.send()` dispatches a string back to the client
+            return context.res.json(transcript)
 
-    # `ctx.res.json()` is a handy helper for sending JSON
-    return context.res.json(
-        {
-            "motto": "Build like a team of hundreds_",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
-        }
-    )
+        # `ctx.res.json()` is a handy helper for sending JSON
+        return context.res.json(
+            {
+                "motto": "Build like a team of hundreds_",
+                "learn": "https://appwrite.io/docs",
+                "connect": "https://appwrite.io/discord",
+                "getInspired": "https://builtwith.appwrite.io",
+            }
+        )
+    except Exception as e:
+        # Handle exceptions here
+        error_message = str(e)
+        context.log(error_message)
+        return context.res.status(500).json({"error": error_message})
